bc5988f02c90c92e0aaced45a3747d3b48d8f6dc
Maksim Zholudev <maximzms@gmail.com>
2022-09-07 11:15:37 +0300

    Make aux functions private

--- sciplot.py
+++ sciplot.py
@@ -26,12 +26,12 @@ class GridFigure:
         self._plot()
         for row, ys in zip(self.axes, yscales):
             for ax, xs in zip(row, xscales):
-                set_scales(ax, xs, ys)
-        self.__apply_to_axes(set_ticks_in)
-        self.__apply_to_axes(remove_xticklabels, row_slice=slice(-1))
-        self.__apply_to_axes(remove_yticklabels, col_slice=slice(1, None))
-        self.__apply_to_axes(tighten_xticklabels, row_slice=-1)
-        self.__apply_to_axes(tighten_yticklabels, col_slice=0)
+                _set_scales(ax, xs, ys)
+        self.__apply_to_axes(_set_ticks_in)
+        self.__apply_to_axes(_remove_xticklabels, row_slice=slice(-1))
+        self.__apply_to_axes(_remove_yticklabels, col_slice=slice(1, None))
+        self.__apply_to_axes(_tighten_xticklabels, row_slice=-1)
+        self.__apply_to_axes(_tighten_yticklabels, col_slice=0)
 
     def _plot(self) -> None:
         pass
@@ -39,39 +39,39 @@ class GridFigure:
     def __apply_to_axes(self, func: Callable[[Axes], None],
             row_slice: slice | int = slice(None),
             col_slice: slice | int = slice(None)) -> None:
-        row_slice = ensure_slice(row_slice)
-        col_slice = ensure_slice(col_slice)
+        row_slice = _ensure_slice(row_slice)
+        col_slice = _ensure_slice(col_slice)
         for row in self.axes[row_slice]:
             for ax in row[col_slice]:
                 func(ax)
 
 
-def remove_xticklabels(ax: Axes) -> None:
+def _remove_xticklabels(ax: Axes) -> None:
     ax.set_xticklabels([])
 
-def remove_yticklabels(ax: Axes) -> None:
+def _remove_yticklabels(ax: Axes) -> None:
     ax.set_yticklabels([])
 
-def set_ticks_in(ax: Axes) -> None:
+def _set_ticks_in(ax: Axes) -> None:
     ax.tick_params(direction="in")
 
-def tighten_xticklabels(ax: Axes) -> None:
+def _tighten_xticklabels(ax: Axes) -> None:
     t = ax.xaxis.get_major_ticks()
     t[0].label.set_horizontalalignment("left")
     t[-1].label.set_horizontalalignment("right")
 
-def tighten_yticklabels(ax: Axes) -> None:
+def _tighten_yticklabels(ax: Axes) -> None:
     t = ax.yaxis.get_major_ticks()
     t[0].label.set_verticalalignment("bottom")
     t[-1].label.set_verticalalignment("top")
 
-def set_scales(ax: Axes, xs: Scale, ys: Scale) -> None:
+def _set_scales(ax: Axes, xs: Scale, ys: Scale) -> None:
     ax.set_xlim(xs[0], xs[-1])
     ax.set_xticks(xs)
     ax.set_ylim(ys[0], ys[-1])
     ax.set_yticks(ys)
 
-def ensure_slice(value: slice | int) -> slice:
+def _ensure_slice(value: slice | int) -> slice:
     if type(value) is slice:
         return value
     if value >= 0:
